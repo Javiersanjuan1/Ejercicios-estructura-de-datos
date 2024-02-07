22d8c76c5561206db67a7371f0c75ba2a169c3cd
Javiersanjuan1 <fsanjua@myuax.com>
2024-02-07 18:15:45 +0100

    Initial commit

---
+++ Ejercicio_2.java
@@ -0,0 +1,16 @@
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+        // Mayor número entero representable en un char
+        char mayorChar = Character.MAX_VALUE;
+        System.out.println("El mayor número entero representable en un char es: " + (int) mayorChar);
+
+        // Mayor número entero representable en un short
+        short mayorShort = Short.MAX_VALUE;
+        System.out.println("El mayor número entero representable en un short es: " + mayorShort);
+
+        // Mayor número entero representable en un int
+        int mayorInt = Integer.MAX_VALUE;
+        System.out.println("El mayor número entero representable en un int es: " + mayorInt);
+    }
+}
+
